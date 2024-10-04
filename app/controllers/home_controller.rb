60907ded73bad90aee79a458dcc316f73d6c985f
Ariel Falcon <arielfalcon21@outlook.com>
2024-10-04 11:01:57 -0500

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def services
+  end
+
+  def gallery
+  end
+  
+  def contact
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/about'
+  get 'home/services'
+  get 'home/gallery'
+  get 'home/contact'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
