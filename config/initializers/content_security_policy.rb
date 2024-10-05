5d95f25bf925cc5e382bb74a2fbbf1c9e94d7719
Ariel Falcon <arielfalcon21@outlook.com>
2024-10-05 11:56:39 -0500

    bug fixes

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -20,7 +20,7 @@
 #
 #   # Generate session nonces for permitted importmap and inline scripts
 #   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src)
+#   config.content_security_policy_nonce_directives = %w[script-src]
 #
 #   # Report violations without enforcing the policy.
 #   # config.content_security_policy_report_only = true
