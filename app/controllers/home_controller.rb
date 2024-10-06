70a0766e8f2f05e10f5d699533a4efa7d55438b7
Ariel Falcon <arielfalcon21@outlook.com>
2024-10-06 15:51:10 -0500

    comments added to home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,28 @@
 
 # This class is the controller for the view pages section.
 class HomeController < ApplicationController
-  def index; end
+  # Function for the Home page.
+  def index
+    # empty function is required to link pages.
+  end
 
-  def about; end
+  # Function for the About page.
+  def about
+    # empty function is required to link pages.
+  end
 
-  def services; end
+  # Function for the Service page.
+  def services
+    # empty function is required to link pages.
+  end
 
-  def gallery; end
+  # Function for the Gallery page.
+  def gallery
+    # empty function is required to link pages.
+  end
 
-  def contact; end
+  # Function for the Contact page.
+  def contact
+    # empty function is required to link pages.
+  end
 end
