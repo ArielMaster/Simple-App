b0750748808966e718e40088519e9a3e64340ea0
Ariel Falcon <arielfalcon21@outlook.com>
2024-10-06 20:41:45 -0500

    update

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   get 'home/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  # Defines the root path route ('/')
   root 'home#index'
 end
